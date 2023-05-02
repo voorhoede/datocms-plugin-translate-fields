836e7026d1468d964b26fd85ab4c4722b7a132f9
velomovies <22169955+velomovies@users.noreply.github.com>
2023-05-02 13:34:38 +0200

    feat(yandex.ts): re-enable yandex translations

--- src/lib/translationServices/yandex.ts
+++ src/lib/translationServices/yandex.ts
@@ -4,23 +4,21 @@ export default async function translate(
   string: string,
   options: TranslationOptions
 ): Promise<string> {
-  // const params = new URLSearchParams()
+  const params = new URLSearchParams()
 
-  // params.set('key', options.apiKey)
-  // params.set('lang', options.toLocale)
-  // params.set('format', 'plain')
-  // params.set('text', string)
+  params.set('key', options.apiKey)
+  params.set('lang', options.toLocale)
+  params.set('format', 'plain')
+  params.set('text', string)
 
-  // const request = await fetch(
-  //   `https://translate.yandex.net/api/v1.5/tr.json/translate?${params.toString()}`
-  // )
+  const request = await fetch(
+    `https://translate.yandex.net/api/v1.5/tr.json/translate?${params.toString()}`
+  )
 
-  // if (request.status !== 200) {
-  //   throw new Error(`Yandex returned status ${request.status}`)
-  // }
+  if (request.status !== 200) {
+    throw new Error(`Yandex returned status ${request.status}`)
+  }
 
-  // const response = await request.json()
-  // return response.text.join(' ')
-
-  return `translated ${string}`
+  const response = await request.json()
+  return response.text.join(' ')
 }
