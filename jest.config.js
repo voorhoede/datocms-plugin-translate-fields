2ad5dcc4c6e6c97906808a5169000f43e44896f9
velomovies <22169955+velomovies@users.noreply.github.com>
2023-05-23 16:51:39 +0200

    feat(jest.config.js): update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const config = {
+  testMatch: ['**/*.test.ts?(x)'],
   transform: {
-    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$':
-      'ts-jest',
+    '^.+\\.(js|jsx|mjs|cjs|ts|tsx)$': 'ts-jest',
   },
   transformIgnorePatterns: ['node_modules/(?!lodash-es/.*)'],
 }
+
+module.exports = config
